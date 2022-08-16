8a25123495574646d6e2fbb951002e566b16ccbd
Ahmed-Imad <ahmedfattal82@gmail.com>
2022-08-17 00:11:20 +0400

    Add project files.

---
+++ Data/WareHouseDbContext.cs
@@ -0,0 +1,11 @@
+﻿using AppWarehouse.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppWareHouse.Data
+{
+    public class WareHouseDbContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
+// ConnectionString has  the following >>> ServerName, DataBaseName, UserName and Password
---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AppWarehouse.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Qty { get; set; }
+        public decimal TotalCost { get; set; }
+        public bool InStock { get; set; }
+        public int BarCode { get; set; }
+        public DateTime CreationDate { get; set; }
+
+    }
+}
