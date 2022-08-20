f322f486d435c55a98b0c27fefb6769728a1e0f4
Ahmed-Imad <ahmedfattal82@gmail.com>
2022-08-20 16:10:35 +0400

    Creating SQL database based on FIRST CODE!

--- Data/WareHouseDbContext.cs
+++ Data/WareHouseDbContext.cs
@@ -1,11 +1,17 @@
 ﻿using AppWarehouse.Models;
+using AppWareHouse.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace AppWareHouse.Data
 {
     public class WareHouseDbContext: DbContext
     {
+        public WareHouseDbContext(DbContextOptions<WareHouseDbContext> options):base(options)
+        {
+
+        }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
 // ConnectionString has  the following >>> ServerName, DataBaseName, UserName and Password
